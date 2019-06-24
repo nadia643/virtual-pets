a660e3351ec889b2ede10f30ce36731591851b91
John Doe <nadia643@gmail.com>
2019-06-24 18:03:02 +0100

    Fido's maximum fitness

--- __tests__/pet.test.js
+++ __tests__/pet.test.js
@@ -6,20 +6,45 @@ describe('constructor', () => {
     });
     
     it('sets the name property', () => {
-          const pet = new Pet('Fido');
-          expect(pet.name).toEqual('Fido');
+        const pet = new Pet('Fido');
+        expect(pet.name).toEqual('Fido');
      });
     it('has an initial age of 0', () => {
         const pet = new Pet('Fido');
         expect(pet.age).toEqual(0);
     });
-    describe('growUp', () => {
-        it('increments age by 1', () => {
+});
+describe('growUp', () => {
+    it('increments age by 1', () => {
         const pet = new Pet('Fido');
         pet.growUp();
         expect(pet.age).toEqual(1);
-        });
+    });
+    it('increments hunger by 5', () => {
+        const pet = new Pet('Fido');
+        pet.growUp();
+        expect(pet.hunger).toEqual(5);  
+    });  
+    it('decreases fitness by 3', () => {
+        const pet = new Pet('Fido');
+        pet.growUp();
+        expect(pet.fitness).toEqual(7);
+    });
+  });
+  describe('walk', () => {
+    it('increases fitness by 4', () => {
+        const pet = new Pet('Fido');
+        pet.fitness = 4;
+        pet.walk();
+        expect(pet.fitness).toEqual(8);
+    });
+    it('increases fitness to a maximum of 10', () => {
+        const pet = new Pet('Fido');
+        const MAXIMUM_FITNESS = 10;
+        pet.fitness = 8;
+        pet.walk();
+        expect(pet.fitness).toEqual(MAXIMUM_FITNESS);
 
     });
-      });
+  });
 
--- src/pet.js
+++ src/pet.js
@@ -1,8 +1,21 @@
 function Pet(name) {
     this.name = name;
     this.age = 0;
-}
-Pet.prototype.growUp = function() {
-    this.age +=1;
+    this.hunger = 0;
+    this.fitness = 10;
 };
+Pet.prototype.growUp = function() {
+    this.age += 1;
+    this.hunger += 5;
+    this.fitness -=3;
+};
+const MAXIMUM_FITNESS = 10;
+Pet.prototype.walk = function() {
+    if ((this.fitness + 4) <= MAXIMUM_FITNESS) {
+        this.fitness += 4;
+    } else {
+        this.fitness = MAXIMUM_FITNESS;
+    }
+}
+
 module.exports = Pet;
